7c5cc082055f95f5907b70e21836b0841751f5a6
DISHA SACHDEVA <87871196+dishasachdeva02@users.noreply.github.com>
2023-11-28 21:11:50 +0530

    Debug

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const fs = require("fs").promises;
 const { google } = require("googleapis");
 
 const port = 8080;
-// these are the scope that we want to access 
 const SCOPES = [
   "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/gmail.send",
@@ -14,31 +13,22 @@ const SCOPES = [
   "https://mail.google.com/",
 ];
 
-// i kept the label name
 const labelName = "Vacation Auto-Reply";
-
-
 app.get("/", async (req, res) => {
 
-  // here i am taking google GMAIL  authentication 
   const auth = await authenticate({
     keyfilePath: path.join(__dirname, "credentials.json"),
     scopes: SCOPES,
   });
 
-  // console.log("this is auth",auth)
 
-  // here i getting authorize gmail id
   const gmail = google.gmail({ version: "v1", auth });
 
 
-  //  here i am finding all the labels availeble on current gmail
   const response = await gmail.users.labels.list({
     userId: "me",
   });
 
-
-  //  this function is finding all email that have unreplied or unseen
   async function getUnrepliesMessages(auth) {
     const gmail = google.gmail({ version: "v1", auth });
     const response = await gmail.users.messages.list({
@@ -50,7 +40,6 @@ app.get("/", async (req, res) => {
     return response.data.messages || [];
   }
 
-  //  this function generating the label ID
   async function createLabel(auth) {
     const gmail = google.gmail({ version: "v1", auth });
     try {
@@ -79,16 +68,10 @@ app.get("/", async (req, res) => {
   }
 
   async function main() {
-    // Create a label for theApp
     const labelId = await createLabel(auth);
-    // console.log(`Label  ${labelId}`);
-    // Repeat  in Random intervals
     setInterval(async () => {
-      //Get messages that have no prior reply
       const messages = await getUnrepliesMessages(auth);
-      // console.log("Unreply messages", messages);
 
-      //  Here i am checking is there any gmail that did not get reply
       if (messages && messages.length > 0) {
         for (const message of messages) {
           const messageData = await gmail.users.messages.get({
@@ -103,7 +86,6 @@ app.get("/", async (req, res) => {
           );
 
           if (!hasReplied) {
-            // Craft the reply message
             const replyMessage = {
               userId: "me",
               resource: {
@@ -127,7 +109,6 @@ app.get("/", async (req, res) => {
 
             await gmail.users.messages.send(replyMessage);
 
-            // Add label and move the email
             await gmail.users.messages.modify({
               auth,
               userId: "me",
@@ -146,7 +127,6 @@ app.get("/", async (req, res) => {
 
   
   main();
-  // const labels = response.data.labels;
   res.json({ "this is Auth": auth });
 });
 
